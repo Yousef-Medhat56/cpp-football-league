f0acb16f85001e13ec3acdbf18c77f7e5cff0418
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2023-05-06 01:33:56 +0300

    test: test "Squad" class

---
+++ test/squad-test.cpp
@@ -0,0 +1,14 @@
+// header files
+#include "../include/squad.h"
+
+// cpp files
+#include "../source/squad.cpp"
+
+int main()
+{
+    Squad squad;
+    cout << "## Print Squad details" << endl;
+    squad.printDetails();
+
+    return 0;
+}
