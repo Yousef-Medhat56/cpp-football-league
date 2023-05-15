370c0d59d8de486de754807718a242970e1fbedf
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2023-05-16 00:19:35 +0300

    test(Match): update test

--- test/match-test.cpp
+++ test/match-test.cpp
@@ -6,15 +6,14 @@
 
 int main()
 {
-    Club club1("Ahly");
-    Club club2("Pyramids");
-    
-   
-    Match match(1,1,&club1,&club2);
-    
+    Club club1(1, "Ahly");
+    Club club2(2, "Pyramids");
+
+    Match match(1, 1, &club1, &club2);
+
     match.setHomeGoals(1);
     match.setAwayGoals(0);
-    
+
     match.printDetails();
     return 0;
 }
