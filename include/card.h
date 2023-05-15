dc227d89154600007f4b58b218d077b3bfeb9bae
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2023-05-16 00:06:01 +0300

    feat(Card): add attributes: player_id, club_id, match_id

--- include/card.h
+++ include/card.h
@@ -4,12 +4,18 @@
 class Card
 {
 private:
+    int player_id;
+    int club_id;
+    int match_id;
     bool is_red; // false: yellow card. true: red card
 
 public:
-    Card();
-    Card(bool);
+    Card(int,int,int,bool);
+
     bool getColor();
+    int getPlayerId();
+    int getClubId();
+    int getMatchId();
     void setColor(bool);
 };
 
--- source/card.cpp
+++ source/card.cpp
@@ -1,11 +1,27 @@
 #include "../include/card.h"
 
-Card::Card() {}
-
-Card::Card(bool val)
+Card::Card(int player_id, int club_id, int match_id, bool val = false)
 {
+    this->player_id = player_id;
+    this->club_id = club_id;
+    this->match_id = match_id;
     this->is_red = val;
 };
+
+int Card::getPlayerId()
+{
+    return this->player_id;
+}
+
+int Card::getClubId()
+{
+    return this->club_id;
+}
+
+int Card::getMatchId()
+{
+    return this->match_id;
+}
 
 bool Card::getColor()
 {
