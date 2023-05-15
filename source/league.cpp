6dd4612189b308cc0020738300c38c6cd49dc324
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2023-05-15 23:54:37 +0300

    feat(League): update the match constructor

--- source/league.cpp
+++ source/league.cpp
@@ -39,9 +39,9 @@ void League::enterClubsDetails()
 
 void League::createMatches()
 {
-    while (gameweek_count < gameweeks_num )
+    while (gameweek_count < gameweeks_num)
     {
-        //First gameweek
+        // First gameweek
         if (gameweek_count == 0)
         {
             Gameweek *first_gameweek = new Gameweek(gameweek_count + 1, matches_num_in_gameweek);
@@ -49,7 +49,7 @@ void League::createMatches()
             {
                 Club *home = clubs[i];
                 Club *away = clubs[clubs_num - i - 1];
-                Match *new_match = new Match(gameweek_count + 1, home, away);
+                Match *new_match = new Match(matches_count + 1, gameweek_count + 1, home, away);
                 matches[i] = new_match;
                 first_gameweek->addMatch(new_match);
                 matches_count++;
@@ -68,7 +68,7 @@ void League::createMatches()
                 // first match in the gameweek
                 Club *first_match_home = (gameweek_count % 2) ? clubs[clubs_num - 1] : last_gameweek_matches[matches_num_in_gameweek - 1]->getAwayTeam();
                 Club *first_match_away = (gameweek_count % 2) ? last_gameweek_matches[matches_num_in_gameweek - 1]->getAwayTeam() : clubs[clubs_num - 1];
-                Match *first_match = new Match(gameweek_count + 1, first_match_home, first_match_away);
+                Match *first_match = new Match(matches_count + 1, gameweek_count + 1, first_match_home, first_match_away);
                 matches[matches_count++] = first_match;
                 new_gameweek->addMatch(first_match);
 
@@ -77,7 +77,7 @@ void League::createMatches()
 
                     Club *home = last_gameweek_matches[i - 2]->getAwayTeam();
                     Club *away = last_gameweek_matches[i - 1]->getHomeTeam();
-                    Match *new_match = new Match(gameweek_count + 1, home, away);
+                    Match *new_match = new Match(matches_count + 1, gameweek_count + 1, home, away);
                     matches[matches_count++] = new_match;
                     new_gameweek->addMatch(new_match);
                 }
@@ -85,7 +85,7 @@ void League::createMatches()
                 // last match in the gameweek
                 Club *last_match_home = (gameweek_count % 2) ? last_gameweek_matches[0]->getHomeTeam() : last_gameweek_matches[0]->getAwayTeam();
                 Club *last_match_away = last_gameweek_matches[1]->getHomeTeam();
-                Match *last_match = new Match(gameweek_count + 1, last_match_home, last_match_away);
+                Match *last_match = new Match(matches_count + 1, gameweek_count + 1, last_match_home, last_match_away);
                 matches[matches_count++] = last_match;
                 new_gameweek->addMatch(last_match);
 
@@ -102,7 +102,7 @@ void League::createMatches()
                     {
                         Club *home = gameWeeks[i]->getMatches()[j]->getAwayTeam();
                         Club *away = gameWeeks[i]->getMatches()[j]->getHomeTeam();
-                        Match *new_match = new Match(gameweek_count + 1, home, away);
+                        Match *new_match = new Match(matches_count + 1, gameweek_count + 1, home, away);
                         matches[matches_count++] = new_match;
                         new_gameweek->addMatch(new_match);
                     }
