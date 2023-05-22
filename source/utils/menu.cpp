29ce57e91f12032d94c74f9dd9b5688ed2d719cf
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2023-05-22 14:10:36 +0300

    feat(Menu): implement header method

---
+++ source/utils/menu.cpp
@@ -0,0 +1,10 @@
+#include "../../include/utils/menu.h"
+
+void Menu::header()
+{
+    Console::clear();
+    Console::divider();
+    cout << "\tFootball League Simulation" << endl;
+    Console::divider();
+    cout << endl;
+}
