cbd8b4c13f7298bdcbd5e42c9c51c03525fe780a
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2023-05-22 12:53:25 +0300

    feat(Match): allow the user to add cards to the players

--- include/match.h
+++ include/match.h
@@ -1,6 +1,8 @@
 #ifndef MATCH_H
 #define MATCH_H
 
+#include <conio.h>
+using namespace std;
 #include "./club.h"
 #include "../source/club.cpp"
 
@@ -16,11 +18,18 @@ private:
     Formation awayFormation;
     int home_goals;
     int away_goals;
+    int home_cards_num;
+    int away_cards_num;
+    int home_cards_count;
+    int away_cards_count;
+    Card **home_cards_list;
+    Card **away_cards_list;
 
 public:
     // Constructor
     Match(int match_id, int gameweek_id, Club *home, Club *away);
 
+    ~Match();
     // Getters
     int getGameWeekNum();
     int getHomeGoals();
@@ -39,10 +48,15 @@ public:
     void printDetails();
     void printHomeFormation();
     void printAwayFormation();
-    Player * getValidPlayer( Player *main_player, Player *substitute_player);
+    Player *getValidPlayer(Player *main_player, Player *substitute_player);
     void pickHomeFormation();
     void pickAwayFormation();
     void enterResults();
+    void enterCards(bool is_home_team);
+    Card *addCardToPlayer( Player *player_ptr);
+    void printPlayerInCardsMenu(int choice_num, Player *player_ptr, int club_cards_count, Card **club_card_list);
+    void chooseCardColor(Card *card);
+    bool checkPlayerGotCard(int player_id, int club_cards_count, Card **club_card_list);
     void updateGoalsStats();
     void determineWinner();
 };
--- source/match.cpp
+++ source/match.cpp
@@ -9,6 +9,12 @@ Match::Match(int match_id, int gameweek_id, Club *home, Club *away)
     this->is_finshed = false;
     this->home_goals = 0;
     this->away_goals = 0;
+    this->home_cards_num = 0;
+    this->away_cards_num = 0;
+    this->home_cards_count = 0;
+    this->away_cards_count = 0;
+    this->home_cards_list = new Card *[3];
+    this->away_cards_list = new Card *[3];
 
     // Home formation
     pickHomeFormation();
@@ -177,6 +183,106 @@ void Match::updateGoalsStats()
     }
 }
 
+void Match::enterCards(bool is_home_team)
+{
+    
+    Club *club_obj = (is_home_team) ? &*home : &*away;
+    Formation *team_formation = (is_home_team) ? &homeFormation : &awayFormation;
+    int *cards_num = (is_home_team) ? &this->home_cards_num : &this->away_cards_num;
+    int *cards_count = (is_home_team) ? &this->home_cards_count : &this->away_cards_count;
+    Card **cards_list = (is_home_team) ? &*this->home_cards_list : &*this->away_cards_list;
+
+    cout << "How many cards did " << club_obj->getName() << " get(0-3): ";
+    cin >> *cards_num;
+
+    if (*cards_num > 0)
+    {
+        while (*cards_count < *cards_num)
+        {
+            Card *new_card;
+
+            printPlayerInCardsMenu(1, team_formation->goalkeeper, *cards_count, cards_list);
+            printPlayerInCardsMenu(2, team_formation->defender, *cards_count, cards_list);
+            printPlayerInCardsMenu(3, team_formation->striker, *cards_count, cards_list);
+
+            cout << "Which player got the card: ";
+            bool exit_loop;
+            do
+            {
+                char c = getch();
+                exit_loop = true;
+                switch (c)
+                {
+                case '1':
+                    if (!checkPlayerGotCard(team_formation->goalkeeper->getId(), *cards_count, cards_list))
+                    {
+                        cout << c << endl;
+                        new_card = this->addCardToPlayer(team_formation->goalkeeper);
+                    }
+                    else
+                        exit_loop = false;
+                    break;
+                case '2':
+                    if (!checkPlayerGotCard(team_formation->defender->getId(), *cards_count, cards_list))
+                    {
+                        cout << c << endl;
+                        new_card = this->addCardToPlayer(team_formation->defender);
+                    }
+                    else
+                        exit_loop = false;
+                    break;
+                case '3':
+                    if (!checkPlayerGotCard(team_formation->striker->getId(), *cards_count, cards_list))
+                    {
+                        cout << c << endl;
+                        new_card = this->addCardToPlayer(team_formation->striker);
+                    }
+                    else
+                        exit_loop = false;
+                    break;
+
+                default:
+                    exit_loop = false;
+                }
+            } while (!exit_loop);
+            cards_list[*cards_count] = new_card;
+            *cards_count += 1;
+        }
+    }
+}
+
+Card *Match::addCardToPlayer(Player *player_ptr)
+{
+    Card *new_card = new Card(player_ptr->getId(), player_ptr->getClubId(), this->id, this->gameweek_id);
+    this->chooseCardColor(new_card);
+    player_ptr->addCard(new_card);
+    return new_card;
+}
+
+void Match::chooseCardColor(Card *card)
+{
+    cout << "Is the card red (y/n): ";
+    bool exit_loop;
+    do
+    {
+        char c = getch();
+        exit_loop = true;
+        switch (c)
+        {
+        case 'y':
+            cout << c << endl;
+            card->setColor(true);
+            break;
+        case 'n':
+            cout << c << endl;
+            card->setColor(false);
+            break;
+        default:
+            exit_loop = false;
+        }
+    } while (!exit_loop);
+}
+
 void Match::enterResults()
 {
     this->is_finshed = true;
@@ -186,4 +292,38 @@ void Match::enterResults()
     cin >> away_goals;
     this->updateGoalsStats();
     this->determineWinner();
+    this->enterCards(true);
+    this->enterCards(false);
+}
+
+bool Match::checkPlayerGotCard(int player_id, int cards_count, Card **cards_list)
+{
+    if (cards_count > 0)
+    {
+        for (int i = 0; i < cards_count; i++)
+        {
+            if (player_id == cards_list[i]->getPlayerId())
+                return true;
+        }
+    }
+    return false;
+}
+
+void Match::printPlayerInCardsMenu(int choice_num, Player *player_ptr, int cards_count, Card **card_list)
+{
+    if (!checkPlayerGotCard(player_ptr->getId(), cards_count, card_list))
+    {
+        cout << choice_num << ". " << player_ptr->getName() << endl;
+    }
+}
+
+Match::~Match()
+{
+    for (int i = 0; i < home_cards_count; i++)
+        delete home_cards_list[i];
+    delete[] home_cards_list;
+
+    for (int i = 0; i < away_cards_count; i++)
+        delete away_cards_list[i];
+    delete[] away_cards_list;
 }
