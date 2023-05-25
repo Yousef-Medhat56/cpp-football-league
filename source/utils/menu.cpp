97d96f677eda3b649490bf2df33ce89012a6bb56
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2023-05-25 17:01:36 +0300

    feat(Menu): hide statistics menu options if the league hasn't started

--- source/utils/menu.cpp
+++ source/utils/menu.cpp
@@ -61,7 +61,7 @@ void Menu::gameweeks(League &league)
     Menu::header();
     cout << "1.Print Gameweeks Table\n";
 
-    //if the league hasn't finished yet
+    // if the league hasn't finished yet
     if (current_gameweek < gameweeks_num)
     {
         cout << "2.Enter Gameweek #" << current_gameweek + 1 << " results\n"
@@ -136,60 +136,73 @@ void Menu::standings(League &league)
 void Menu::statistics(League &league)
 {
     Menu::header();
-    cout << "1.Print Top 3 Goalkeepers\n"
-         << "2.Print Top 3 Defenders\n"
-         << "3.Print Top 3 Strikers\n"
-         << "4.Print Top 3 Carded Players\n"
-         << "0.Go back\n"
-         << "Enter your choice: ";
-
-    bool exit_loop;
-    do
+    //if the league hasn't started yet
+    if (league.getCurrentGameweekNum() == 0)
     {
-        exit_loop = true;
-        switch (getch())
+        //ask the user to enter the results of gameweek #1
+        Console::error("Please enter results of Gameweek #1 first");
+        // go back to the main menu
+        Menu::footer(league, [](League &league)
+                     { main(league); });
+    }
+    else
+    {
+        cout << "1.Print Best Goalkeepers\n"
+             << "2.Print Best Defenders\n"
+             << "3.Print Best Strikers\n"
+             << "4.Print Top Carded Players\n"
+             << "0.Go back\n"
+             << "Enter your choice: ";
+
+        bool exit_loop;
+        do
         {
-        case '1':
-            Menu::header();
-            league.printTopGoalkeepers();
+            exit_loop = true;
+            switch (getch())
+            {
+            case '1':
+                Menu::header();
+                league.printTopGoalkeepers();
 
-            // go back to the statistics menu
-            Menu::footer(league, [](League &league)
-                         { statistics(league); });
-            break;
-        case '2':
-            Menu::header();
-            league.printTopDefenders();
+                // go back to the statistics menu
+                Menu::footer(league, [](League &league)
+                             { statistics(league); });
+                break;
+            case '2':
+                Menu::header();
+                league.printTopDefenders();
 
-            // go back to the statistics menu
-            Menu::footer(league, [](League &league)
-                         { statistics(league); });
-            break;
-        case '3':
-            Menu::header();
-            league.printTopStrikers();
+                // go back to the statistics menu
+                Menu::footer(league, [](League &league)
+                             { statistics(league); });
+                break;
+            case '3':
+                Menu::header();
+                league.printTopStrikers();
 
-            // go back to the statistics menu
-            Menu::footer(league, [](League &league)
-                         { statistics(league); });
-            break;
-        case '4':
-            Menu::header();
-            league.printTopCarded();
+                // go back to the statistics menu
+                Menu::footer(league, [](League &league)
+                             { statistics(league); });
+                break;
+            case '4':
+                Menu::header();
+                league.printTopCarded();
 
-            // go back to the statistics menu
-            Menu::footer(league, [](League &league)
-                         { statistics(league); });
-            break;
+                // go back to the statistics menu
+                Menu::footer(league, [](League &league)
+                             { statistics(league); });
+                break;
 
-        case '0':
-            Menu::main(league);
-            break;
-        default:
-            exit_loop = false;
-        }
-    } while (!exit_loop);
+            case '0':
+                Menu::main(league);
+                break;
+            default:
+                exit_loop = false;
+            }
+        } while (!exit_loop);
+    }
 }
+
 void Menu::clubs(League &league)
 {
     Menu::header();
